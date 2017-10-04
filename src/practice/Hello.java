5eef21f89a9a911f59a51a8774b28dc8d77c3f02
diamo <diamo@LAPTOP-1VBV74R7.attlocal.net>
2017-10-04 04:21:51 -0500

    Temps

---
+++ src/practice/Hello.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println(temps( -3, 101));
+	}
+	
+		static boolean temps(int a, int b) {
+	
+			if( a < 0 &&  b > 100){
+		return true;
+	}
+	else
+		return false;
+}
+	}
